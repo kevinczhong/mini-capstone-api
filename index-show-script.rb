da0ef578edbd40df92c06029ca29a847bf391b36
Kevin Zhong <kvnzhong@gmail.com>
2022-05-04 11:31:12 -0700

    Implemented testing methods for index/show in testing script; now displaying in terminal.

--- index-show-script.rb
+++ index-show-script.rb
@@ -1,4 +1,12 @@
 require "http"
-response = HTTP.get("http://localhost:3000/products")
-index = response.parse(:json)
-pp index
+puts "If you would like the index, please type in index. Else, please type in the id of the product you would like to access the information of."
+pref = gets.chomp
+if pref == "index"
+  response = HTTP.get("http://localhost:3000/products")
+  index = response.parse(:json)
+  pp index
+else
+  response = HTTP.get("http://localhost:3000/products" + "/#{pref.to_i}")
+  show = response.parse(:json)
+  pp show
+end
