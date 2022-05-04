87e30332289e067a38e24e2e61341e5a3bb09214
Kevin Zhong <kvnzhong@gmail.com>
2022-05-04 10:16:36 -0700

    Implemented tty-table support.

--- products-terminal-script.rb
+++ products-terminal-script.rb
@@ -1,14 +1,20 @@
 require "http"
 require "tty-table"
 
-puts "Would you like to pull the information for one product, or all products?"
-pref = gets.chomp
-if pref == "all_products"
-  response = HTTP.get("http://localhost:3000/all_products")
-  all_products = response.parse(:json)
-  pp all_products
-else
-  response = HTTP.get("http://localhost:3000/#{pref}")
-  product = response.parse(:json)
-  pp product
-end
+# puts "Would you like to pull the information for one product, or all products?"
+# pref = gets.chomp
+# if pref == "all_products"
+#   response = HTTP.get("http://localhost:3000/all_products")
+#   all_products = response.parse(:json)
+#   pp all_products
+# else
+#   response = HTTP.get("http://localhost:3000/#{pref}")
+#   product = response.parse(:json)
+#   pp product
+# end
+
+response = HTTP.get("http://localhost:3000/all_products")
+all_products = response.parse(:json)["products"]
+product_table = TTY::Table.new(["Name", "Price"], [["#{all_products[0]["name"]}", "#{all_products[0]["price"]}"], ["#{all_products[1]["name"]}", "#{all_products[1]["price"]}"], ["#{all_products[2]["name"]}", "#{all_products[2]["price"]}"]])
+
+puts product_table.render(:ascii)
