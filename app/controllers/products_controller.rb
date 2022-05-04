d355dc27494c4e0b9d6d8f84047811b523bcc13a
Kevin Zhong <kvnzhong@gmail.com>
2022-05-04 09:37:24 -0700

    Created provisional all_products method to display information for all products in the database.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,5 @@
 class ProductsController < ApplicationController
+  def all_products
+    render json: { products: Product.all }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/products", controller: "products", action: "product_info"
+  get "/all_products", controller: "products", action: "all_products"
 end
