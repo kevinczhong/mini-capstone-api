5b5a935d8e44cf46017c6f8e72e0a541e05c7f62
Kevin Zhong <kvnzhong@gmail.com>
2022-05-04 13:37:14 -0700

    Created basic framework for a update method in the frontend script.

--- index-show-script.rb
+++ index-show-script.rb
@@ -1,12 +1,22 @@
 require "http"
-puts "If you would like the index, please type in index. Else, please type in the id of the product you would like to access the information of."
+# puts "If you would like the index, please type in index. Else, please type in the id of the product you would like to access the information of."
+# pref = gets.chomp
+# if pref == "index"
+#   response = HTTP.get("http://localhost:3000/products")
+#   index = response.parse(:json)
+#   pp index
+# else
+#   response = HTTP.get("http://localhost:3000/products" + "/#{pref.to_i}")
+#   show = response.parse(:json)
+#   pp show
+# end
+
+response = HTTP.get("http://localhost:3000/products/3")
+update = response.parse(:json)
+puts "What would you like to update?"
 pref = gets.chomp
-if pref == "index"
-  response = HTTP.get("http://localhost:3000/products")
-  index = response.parse(:json)
-  pp index
-else
-  response = HTTP.get("http://localhost:3000/products" + "/#{pref.to_i}")
-  show = response.parse(:json)
-  pp show
+if pref == "name"
+elsif pref == "price"
+elsif pref == "image_url"
+elsif pref == "description"
 end
