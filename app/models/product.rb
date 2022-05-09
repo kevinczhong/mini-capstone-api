0f3d0787a175ba1913a17092207e3e32f0c9d1b1
Kevin Zhong <kvnzhong@gmail.com>
2022-05-09 10:18:14 -0700

    Added image placeholder check to model methods.

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,23 @@ class Product < ApplicationRecord
     end
   end
 
+  def is_shoe?
+    check = name.split(" ")
+    if check[1] == "Shoes"
+      return true
+    else
+      return false
+    end
+  end
+
+  def image_todo
+    if image_url == "placeholder"
+      return "This product still needs an image."
+    else
+      return "Image ready."
+    end
+  end
+
   def tax
     tax = price.to_f * 0.09
     tax = tax.truncate(2)
