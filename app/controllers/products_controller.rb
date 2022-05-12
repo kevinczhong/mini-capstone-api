597d3e27b135ea21f8cdd6376d4a5b8ec303906d
Kevin Zhong <kvnzhong@gmail.com>
2022-05-12 11:31:28 -0700

    Removed vestigial image_url methods from product create and update methods.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,6 @@ class ProductsController < ApplicationController
     product = Product.new(
       name: params["name"],
       price: params["price"],
-      images_url: params["image_url"],
       description: params["description"],
       supplier_id: params["supplier_id"],
       # name: "Pants",
@@ -39,7 +38,6 @@ class ProductsController < ApplicationController
     product.update(
       name: params["name"] || product.name,
       price: params["price"] || product.price,
-      image_url: params["image_url"] || product.image_url,
       description: params["description"] || product.description,
       supplier_id: params["supplier_id"] || product.supplier_id,
     )
