678b8121ee83fe11ffb07b6358dd66577e519fd1
Kevin Zhong <kvnzhong@gmail.com>
2022-05-18 09:10:33 -0700

    Forgot to commit. Committing stuff.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,13 +4,14 @@ class Category < ApplicationRecord
 
   # def products
   #   # Non-functional
-  #   # products_array = []
-  #   # for category_product in category_products
-  #   #   if category_product.category
-  #   #     products_array << category_product.product
-  #   #   end
-  #   # end
-  #   products_array = category_products.map { |category_product| category_product.product }
+  #   products_array = []
+  #   for category_product in category_products
+  #     if category_product.category_id == id
+  #       products_array << category_product.product
+  #     end
+  #   end
   #   return products_array
+  #   # products_array = category_products.map { |category_product| category_product.product }
+  #   # return products_array
   # end
 end
